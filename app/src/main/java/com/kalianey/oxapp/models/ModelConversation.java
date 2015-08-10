ab491a0ab62310c14e8dcb45762e227fef8cc9cd
Kalianey <kalianey@gmail.com>
2015-08-10 15:46:41 +0200

    Initial Commit

---
+++ app/src/main/java/com/kalianey/oxapp/models/ModelConversation.java
@@ -0,0 +1,105 @@
+package com.kalianey.oxapp.models;
+
+/**
+ * Created by kalianey on 10/08/2015.
+ */
+public class ModelConversation {
+
+    private String id;
+    private String opponentId;
+    private String name;
+    private String avatarUrl;
+    private String previewText;
+    private String timeLabel;
+    private String conversationViewed;
+    private String conversationRead;
+    private String conversationReplied;
+    private String onlineStatusBool;
+    private String onlineStatusString;
+    private String newMessageCount;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOpponentId() {
+        return opponentId;
+    }
+    public void setOpponentId(String opponentId) {
+        this.opponentId = opponentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatarUrl(){
+        return avatarUrl;
+    }
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getPreviewText(){
+        return previewText;
+    }
+    public void setPreviewText(String previewText) {
+        this.previewText = previewText;
+    }
+
+    public String getTimeLabel() {
+        return timeLabel;
+    }
+    public void setTimeLabel(String timeLabel){
+        this.timeLabel = timeLabel;
+    }
+
+    public String getConversationViewed() {
+        return conversationViewed;
+    }
+    public void setConversationViewed(String conversationViewed) {
+        this.conversationViewed = conversationViewed;
+    }
+
+    public String getConversationRead() {
+        return conversationRead;
+    }
+    public void setConversationRead(String conversationRead){
+        this.conversationRead = conversationRead;
+    }
+
+    public String getconversationReplied() {
+        return conversationReplied;
+    }
+    public void setconversationReplied(String conversationReplied){
+        this.conversationReplied = conversationReplied;
+    }
+
+    public String getonlineStatusBool() {
+        return onlineStatusBool;
+    }
+    public void setonlineStatusBool(String onlineStatusBool){
+        this.onlineStatusBool = onlineStatusBool;
+    }
+
+    public String getonlineStatusString() {
+        return onlineStatusString;
+    }
+    public void setonlineStatusString(String onlineStatusString){
+        this.onlineStatusString = onlineStatusString;
+    }
+
+    public String getnewMessageCount() {
+        return newMessageCount;
+    }
+    public void setnewMessageCount(String newMessageCount){
+        this.newMessageCount = newMessageCount;
+    }
+    
+}
---
+++ app/src/main/java/com/kalianey/oxapp/views/ConversationListFragment.java
@@ -0,0 +1,24 @@
+package com.kalianey.oxapp.views;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kalianey.oxapp.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ConversationListFragment extends Fragment {
+
+    public ConversationListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_conversation_list, container, false);
+    }
+}
