34e11c03ca4b5f675ae7271f249c81baa952e7c2
Kalianey <kalianey@gmail.com>
2015-08-12 19:58:45 +0200

    Test with styles and drawable chat

--- app/src/main/java/com/kalianey/oxapp/views/MessageFragment.java
+++ app/src/main/java/com/kalianey/oxapp/views/MessageFragment.java
@@ -19,6 +19,8 @@ public class MessageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+
+        return view;
     }
 }
