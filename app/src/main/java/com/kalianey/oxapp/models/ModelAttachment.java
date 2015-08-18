bc4c526c3ace3fdde1a227bc5849c8bd23a0e736
Kalianey <kalianey@gmail.com>
2015-08-18 15:36:36 +0200

    Added a pinterest like grid view on profile https://github.com/etsy/AndroidStaggeredGrid

---
+++ app/src/main/java/com/kalianey/oxapp/models/ModelAttachment.java
@@ -0,0 +1,7 @@
+package com.kalianey.oxapp.models;
+
+/**
+ * Created by kalianey on 18/08/2015.
+ */
+public class ModelAttachment {
+}
---
+++ app/src/main/java/com/kalianey/oxapp/views/adapters/ProfilePhotoGridAdapter.java
@@ -0,0 +1,7 @@
+package com.kalianey.oxapp.views.adapters;
+
+/**
+ * Created by kalianey on 18/08/2015.
+ */
+public class ProfilePhotoGridAdapter {
+}
